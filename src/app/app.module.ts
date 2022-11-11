068c399f2273e20069dda2dce229616d00dc0439
LucaSantosSP <54375485+LucaSantosSP@users.noreply.github.com>
2022-11-11 11:44:48 -0300

    Adicionado contador na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { DisciplinasComponent } from './components/disciplinas/disciplinas.component';
+import { DisciplinasService } from './services/disciplinas.service';
+import { TimerService } from './services/timer.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: TimerComponent },
+      { path: 'disciplinas', component: DisciplinasComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TimerComponent,
+    DisciplinasComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [DisciplinasService, TimerService],
 })
-export class AppModule { }
+export class AppModule {}
